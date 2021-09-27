ae87162b3d4349d42d25c8a58d525b7c1b7e1849
DxrosNgu <diego.267.ri@gmail.com>
2021-09-26 20:10:22 -0500

    SPRINT 01: Adding configuration projects

---
+++ meet-congress-config-server/src/test/java/com/pe/stmp/one/meetcongressconfigserver/MeetCongressConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.stmp.one.meetcongressconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetCongressConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
