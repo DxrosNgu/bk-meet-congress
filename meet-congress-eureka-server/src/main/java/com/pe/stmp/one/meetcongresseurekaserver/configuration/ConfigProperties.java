936bbeae02931030e3a03f1236ea8a5ce62de7e6
DxrosNgu <diego.267.ri@gmail.com>
2021-10-03 15:30:34 -0500

    Modify a few properties files

---
+++ meet-congress-eureka-server/src/main/java/com/pe/stmp/one/meetcongresseurekaserver/configuration/ConfigProperties.java
@@ -0,0 +1,7 @@
+package com.pe.stmp.one.meetcongresseurekaserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigProperties {
+}
---
+++ meet-congress-zuul-server/src/main/java/com/pe/stmp/one/meetcongresszuulserver/configuration/ConfigProperties.java
@@ -0,0 +1,7 @@
+package com.pe.stmp.one.meetcongresszuulserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigProperties {
+}
