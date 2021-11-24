c1a2bdc3f1481205e665a4154c8ed13a248f119d
DxrosNgu <diego.267.ri@gmail.com>
2021-11-24 01:09:39 -0500

    SPRINT 01: update properties and configuration

--- meet-congress-eureka-server/src/main/java/com/pe/stmp/one/meetcongresseurekaserver/configuration/ConfigProperties.java
+++ meet-congress-eureka-server/src/main/java/com/pe/stmp/one/meetcongresseurekaserver/configuration/ConfigProperties.java
@@ -1,7 +1,10 @@
 package com.pe.stmp.one.meetcongresseurekaserver.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties
 public class ConfigProperties {
 }
