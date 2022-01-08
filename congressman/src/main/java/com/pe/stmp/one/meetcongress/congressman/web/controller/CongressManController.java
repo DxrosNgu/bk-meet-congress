976c31dcbd97cdc5b53852fe09fedab1989d22ba
DxrosNgu <diego.267.ri@gmail.com>
2022-01-08 00:33:24 -0500

    add getmember

--- congressman/src/main/java/com/pe/stmp/one/meetcongress/congressman/web/controller/CongressManController.java
+++ congressman/src/main/java/com/pe/stmp/one/meetcongress/congressman/web/controller/CongressManController.java
@@ -7,10 +7,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import java.util.List;
 
 @RefreshScope
@@ -24,6 +23,12 @@ public class CongressManController {
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Congressman>> getListCongressMan(){
         return new ResponseEntity<>(congressManService.getListCongressman(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-member/{id}",
+    consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Congressman> getCongresmman(@PathVariable(value = "id") String idMember){
+        return null;
     }
 
     @PostMapping(value = "/insert-image", consumes = MediaType.APPLICATION_JSON_VALUE,
